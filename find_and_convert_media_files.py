8f9923f669bd916cae490432f448f6d0697d2c77
beliefs22 <beliefs22@gmail.com>
2017-10-16 10:39:43 -0400

    Cleaned up find_and_convert_media_files some. Fixed function parameters and reformatted code with pycharm

--- find_and_convert_media_files.py
+++ find_and_convert_media_files.py
@@ -71,7 +71,6 @@ def search_folders(folder, file_type):
     print("Searching {} for {} type files".format(folder, file_type))
     items = os.listdir(folder)
 
-
     for item in items:
         full_item = os.path.join(folder, item)
         if os.path.isdir(full_item):
@@ -82,7 +81,7 @@ def search_folders(folder, file_type):
             # for m in matches:
             #     yield m
 
-    # return all_matches
+            # return all_matches
 
 
 def search_file(filename, file_type):
@@ -97,18 +96,19 @@ def search_file(filename, file_type):
         yield filename[: len(filename) - len(file_type) - 1]
 
 
-def convert_video_to_mp4(video, file_type_to_convert, input_folder,  dir_to_save=None):
+def convert_video_to_mp4(video, file_type_to_convert, input_folder, dir_to_save=None):
     print("video is", video)
     if dir_to_save:
         print("inside conver, we got a directory", dir_to_save)
         new_video = dir_to_save + os.sep + video.replace(file_type_to_convert, "mp4").replace(input_folder, "")
         print(new_video, "insde of dir to save opt")
-    else: # save in current directory
+    else:  # save in current directory
         new_video = video.replace(file_type_to_convert, "mp4")
     print("New video will be saved to {}".format(new_video))
-    subprocess.run(['ffmpeg', '-i', video, "-strict","-2", new_video], stdout=subprocess.PIPE)
+    subprocess.run(['ffmpeg', '-i', video, "-strict", "-2", new_video], stdout=subprocess.PIPE)
 
-def find_and_convert_media_files(input_folder, file_type, output_folder):
+
+def find_and_convert_media_files(input_folder, file_type, output_folder=None):
     """
     Locates media files or the given type in the given folder and converts them to mp4 format
     :param input_folder: directory that contains the media files
@@ -117,38 +117,36 @@ def find_and_convert_media_files(input_folder, file_type, output_folder):
     :return:
     """
     # TODO: Add options for different file types this can serve as the skelton for each type
-    if len(sys.argv) == 4:
-        print_header()
-        folder = input_folder
-        folder = get_folder_from_user(folder)
-        if not folder:
-            print(folder)
-            print("Sorry we can't search that location.")
-            return
-        file_type = file_type
+    print_header()
+    folder = input_folder
+    folder = get_folder_from_user(folder)
+    if not folder:
+        print(folder)
+        print("Sorry we can't search that location.")
+        return
+    file_type = file_type
 
-        if not file_type.strip():
-            print("We can't search for nothing!")
-            return
+    if not file_type.strip():
+        print("We can't search for nothing!")
+        return
 
-        print("Searching {} for {} type files".format(folder, file_type))
+    print("Searching {} for {} type files".format(folder, file_type))
 
-        matches = search_folders(folder, file_type)
-        if output_folder:
-            dir_to_save = output_folder
-            dir_to_save = get_folder_from_user(dir_to_save)
-            if dir_to_save:
-                for m in matches:
-                    print(m)
-                    convert_video_to_mp4("{}.{}".format(m, file_type), file_type, folder, dir_to_save=dir_to_save)
-            else:
-                print("Sorry we can't search there")
-        else:
+    matches = search_folders(folder, file_type)
+    if output_folder:
+        dir_to_save = output_folder
+        dir_to_save = get_folder_from_user(dir_to_save)
+        if dir_to_save:
             for m in matches:
                 print(m)
-                convert_video_to_mp4("{}.{}".format(m, file_type), file_type, folder)
+                convert_video_to_mp4("{}.{}".format(m, file_type), file_type, folder, dir_to_save=dir_to_save)
+        else:
+            print("Sorry we can't search there")
     else:
-        print("You did not provide the correct arguments inpupt_dir file_type [out_directory]")
-        return
+        for m in matches:
+            print(m)
+            convert_video_to_mp4("{}.{}".format(m, file_type), file_type, folder)
+
+
 if __name__ == '__main__':
     main()
