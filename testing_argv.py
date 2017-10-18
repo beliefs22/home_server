6adf5c4a2f2303ef9d9ffc9aeb1a72f2d23e5cf0
beliefs22 <beliefs22@gmail.com>
2017-10-18 14:11:56 -0400

    Added argparse to parse the arguments when called from command line. Added ability for user to determine number of threads they would like to use.

--- testing_argv.py
+++ testing_argv.py
@@ -1,6 +1,11 @@
+import argparse
 import sys
 
-arguments = sys.argv
-print(arguments)
-for arg in arguments:
-    print(arg)
+parser = argparse.ArgumentParser()
+parser.add_argument('vin', help='directory to search videos for.')
+parser.add_argument('ftype', help='video type to search for')
+parser.add_argument('-vout', help='directory to save converted video too. If blank will save in vin')
+parser.add_argument('-threads', type=int, choices=range(1, 9), help='Number of threads to limit ffmpeg to')
+
+args = parser.parse_args(sys.argv[1:])
+print(args.vin, args.ftype, args.vout, args.threads)
